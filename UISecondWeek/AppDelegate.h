c4a272737e9e90c836f6b2765eb35fe735dfe81d
Jun Marck Rael Legion <jun.legion@opsolutions.biz>
2019-11-04 13:10:00 +0800

    Initial Commit

---
+++ UISecondWeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISecondWeek
+//
+//  Created by OPS on 11/4/19.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISecondWeek/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UISecondWeek
+//
+//  Created by OPS on 11/4/19.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
